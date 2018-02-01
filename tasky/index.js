691a67a65b20384b8f1a10da3ac06b8402c709f2
Elton Diego Garbin do Nascimento <eltongarbin@gmail.com>
2018-02-01 19:45:35 -0200

    MainWindow Class

--- tasky/index.js
+++ tasky/index.js
@@ -1,8 +1,9 @@
 const path = require('path');
 const electron = require('electron');
 const TimerTray = require('./app/timer_tray');
+const MainWindow = require('./app/main_window');
 
-const { app, BrowserWindow } = electron;
+const { app } = electron;
 
 let mainWindow;
 let tray;
@@ -11,17 +12,7 @@ app.on('ready', () => {
   if (process.platform === 'darwin')
     app.dock.hide();
 
-  mainWindow = new BrowserWindow({
-    height: 500,
-    width: 300,
-    frame: false,
-    resizable: false,
-    show: false
-  });
-  mainWindow.loadURL(`file://${__dirname}/src/index.html`);
-  mainWindow.on('blur', () => {
-    mainWindow.hide();
-  });
+  mainWindow = new MainWindow(`file://${__dirname}/src/index.html`);
 
   const iconName = process.platform === 'win32' ? 'windows-icon.png' : 'iconTemplate.png';
   const iconPath = path.join(__dirname, `./src/assets/${iconName}`);
