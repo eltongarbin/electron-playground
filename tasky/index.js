333ce9012cf68f5d45a5968ccf370f43d059cb26
Elton Diego Garbin do Nascimento <eltongarbin@gmail.com>
2018-02-01 17:37:52 -0200

    Setting Position with Bounds

--- tasky/index.js
+++ tasky/index.js
@@ -20,10 +20,21 @@ app.on('ready', () => {
   const iconPath = path.join(__dirname, `./src/assets/${iconName}`);
 
   tray = new Tray(iconPath);
-  tray.on('click', () => {
+  tray.on('click', (event, bounds) => {
+    const { x, y } = bounds;
+    const { height, width } = mainWindow.getBounds();
+
     if (mainWindow.isVisible()) {
       mainWindow.hide();
     } else {
+      const yPosition = process.platform === 'darwin' ? y : y - height;
+      
+      mainWindow.setBounds({
+        x: x - (width / 2),
+        y: yPosition,
+        height,
+        width
+      });
       mainWindow.show();
     }
   });
